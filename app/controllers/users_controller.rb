5782610a04ecf91611b107205ffd3e31151841c0
Max Findel <max@findel.cl>
2012-03-23 17:42:27 -0400

    recaptcha e intereses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,7 +67,7 @@ class UsersController < ApplicationController
         if !simple_captcha_valid?
           gflash :error => 'No has ingresado el ReCaptcha correctamente'
         end
-        return render edit_user_path(@user.id)
+        return redirect_to edit_user_path(@user.id)
       end
     end
   end
