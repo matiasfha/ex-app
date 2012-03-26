b6aa1f6a1da86c6820731de3c4d4072af982bd7c
Max Findel <max@findel.cl>
2012-03-25 20:22:34 -0400

    assets

--- app/admin/interests.rb
+++ app/admin/interests.rb
@@ -2,8 +2,11 @@ ActiveAdmin.register Interest do
   
   index do
 
-    column "Name", :sortable => :last_name do |interest|
+    column "Name", :sortable => :name do |interest|
       link_to interest.name.html_safe, admin_interest_path(interest)
+    end
+    column "Users that like this" do |interest|
+      interest.users.count.to_s
     end
     default_actions
   end
