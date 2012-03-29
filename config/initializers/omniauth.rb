13635583e15723c36626a83177d5ba5df43ea935
Max Findel <max@findel.cl>
2012-03-29 15:09:44 -0400

    oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '324006000982228', '739e9a262d2101043b0afe9a8d266733'
-  provider :twitter, 'AG8uAgQCV15AbjPc9cRlYQ', 'velTM3lvxgttbSWXjSzRaiIDGnc9GNTcmda9BAJM'
+  provider :facebook, '374295515926917', 'c79528011ea1de9413185ef0333a69ab'
+  provider :twitter, 'dTMGX2ISNeEE7Sy5r1iedA', 'TdbDYXm6NF5PA7l6vGoQXQjZFr3uoqKEUklVuqKiieU'
 end
