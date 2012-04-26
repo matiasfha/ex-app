0cf874dafff2b1605bc91399c2d63cea012c26d7
Max Findel <max@findel.cl>
2012-04-26 19:01:57 +0200

    new video bug

--- app/models/video.rb
+++ app/models/video.rb
@@ -8,14 +8,14 @@ class Video < ActiveRecord::Base
   has_many :experiments, :through => :user_experiment_videos
 
 
-  has_attached_file :content, :styles => { :medium => "300x300>", :thumb => "64x64>" },   
+  has_attached_file :content   
                     :storage => :s3,
                     :bucket => 'dandoo-videos',
                     :s3_credentials => {
                       :access_key_id => 'AKIAJK5NVAGQBRCX4GQA',
                       :secret_access_key => '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'
                     },
-                    :path => ":attachment/:id/:style.:basename.:extension"
+                    :path => ":attachment/:id/:basename.:extension"
 
   has_attached_file :preview, :styles => { :medium => "300x300>", :thumb => "64x64>" },   
                     :storage => :s3,
