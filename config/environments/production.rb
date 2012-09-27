e61a117847647317c8ebdef116e29b45819ca229
Matias Hernandez Arellano <matiasfh@gmail.com>
2012-09-26 23:34:40 -0300

    Fix 6 asset precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ DandooDev::Application.configure do
 
   
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( *.js, *.css )
+  config.assets.precompile += %w( *.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
