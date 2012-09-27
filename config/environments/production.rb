853c8388a5e075d03a8b93ac9bdd5b9f9bef9a65
Matias Hernandez Arellano <matias@dandoo.tv>
2012-09-27 14:43:26 -0300

    Revemo Dalli and rubyracer from Gemfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ DandooDev::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_controller.asset_host = ENV['ASSET_HOST']
+  # config.action_controller.asset_host = ENV['ASSET_HOST']
 
   
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
