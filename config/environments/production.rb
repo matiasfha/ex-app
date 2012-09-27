8effb9f346f6d805540edbe72e998ab95a975a27
Matias Hernandez Arellano <matias@dandoo.tv>
2012-09-27 12:53:15 -0300

    Fix assets precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ DandooDev::Application.configure do
   config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
   # Generate digests for assets URLs
   config.assets.digest = true
 
@@ -49,7 +49,7 @@ DandooDev::Application.configure do
 
   
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w(home.js.coffee,user.js.coffee,picture.js.coffee)
+  config.assets.precompile += %w(*.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
