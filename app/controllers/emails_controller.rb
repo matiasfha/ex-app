9ce4b8c1cdcaacf71f7430fdbd0936d8f65d1140
Matias Hernandez Arellano <matiasfh@gmail.com>
2012-10-14 01:59:19 -0300

    Agregado recaptcha

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,15 +1,34 @@
+require "uri"
+require "net/http"
+
 class EmailsController < ApplicationController
   def new
   	@email = Email.new
   end
 
+
+  def check_captcha(challenge,response)
+    params = {
+      :privatekey => ENV['RECAPTCHA_PRIVATE'],
+      :remoteip => request.remote_ip,
+      :challenge => challenge,
+      :response  => response
+    }
+    x = Net::HTTP.post_form(URI.parse('http://www.google.com/recaptcha/api/verify'), params)
+    x.body
+  end
   def create
-  	@email = Email.where(:email => params[:email][:email]).first
-  	if @email.nil?
-  		@email = Email.create! params[:email]
-  		render :json => {:success => @email.save,:mensaje => 'OK'}
-  	else
-  		render :json => {:mensaje => 'Ya Existe',:success => 'repeat'}
-  	end
+    if check_captcha(params[:recaptcha_challenge_field],params[:recaptcha_response_field])
+      @email = Email.where(:email => params[:email][:email]).first
+      if @email.nil?
+        @email = Email.new params[:email]
+        render :json => {:success => @email.save,:mensaje => 'OK'}
+      else
+        render :json => {:mensaje => 'Ya Existe',:success => 'repeat'}
+      end
+    else
+      render :json => {:success => 'recaptcha'}
+    end
+  	
   end
 end
