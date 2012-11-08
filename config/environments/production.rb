b1f3ca263bfff7e9e63362cc7541d90608247675
Matias Hernandez Arellano <matiasfh@gmail.com>
2012-11-08 13:26:31 -0300

    Deploy Version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,15 +12,16 @@ DandooDev::Application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
+  config.assets.debug = true
   config.assets.css_compressor = :yui
   config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
   # Generate digests for assets URLs
   config.assets.digest = true
-
+  
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
