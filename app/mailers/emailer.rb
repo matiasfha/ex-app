7bbe8b71895adcdb891f5092ab612c84fe0ea9ae
Matias Hernandez Arellano <matiasfh@gmail.com>
2012-11-21 13:38:24 -0300

    Emailer para Feedback

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -1,7 +1,7 @@
 class Emailer < ActionMailer::Base
 	def feedback_email(autor, from, comentario)
 		@comentario = comentario
-		mail(:to => 'matiasfh@gmail.com',
+		mail(:to => 'soporte@dandoo.tv',
 			:subject => "Feedback [#{autor}]",
 			:from =>  from
 		)
