5160e2e855886c549ec88b0e6ff6bfbd87742f7c
Matias Hernandez Arellano <matiasfh@gmail.com>
2012-12-07 12:18:17 -0300

    Validacion al subir

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -3,6 +3,15 @@ class ResourcesController < ApplicationController
 	before_filter :authenticate_user!, :only => [:create,:destroy,:subir]
 	respond_to :html
 
+	def http_referer_uri
+	  request.env["HTTP_REFERER"] && URI.parse(request.env["HTTP_REFERER"])
+	end
+
+	def refered_from_our_site?
+	  if uri = http_referer_uri
+	    uri.host == request.host
+	  end
+	end
 	
 
 	def show
