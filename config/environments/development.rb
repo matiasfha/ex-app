1884c1b0a74fa3d1afdc92c68612f6415b37a072
Matias Hernandez Arellano <matiasfh@gmail.com>
2012-12-16 16:19:54 -0300

    New cache based on basecamp key-value

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ DandooDev::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true#false
 
   # Don't care if the mailer can't send
   #config.action_mailer.raise_delivery_errors = false
@@ -57,11 +57,11 @@ DandooDev::Application.configure do
   end
 
   # Enable Dalli in config/environments/production.rb:
-  # config.perform_caching = true
-  # config.cache_store = :dalli_store, 'localhost:11211'
-  # config.action_dispatch.rack_cache = {
-  #   :metastore    => Dalli::Client.new,
-  #   :entitystore  => 'file:/tmp/cache/rack/body',
-  #   :allow_reload => false
-  # }
+  config.perform_caching = true
+  config.cache_store = :dalli_store, 'localhost:11211'
+  config.action_dispatch.rack_cache = {
+    :metastore    => Dalli::Client.new,
+    :entitystore  => 'file:/tmp/cache/rack/body',
+    :allow_reload => false
+  }
 end
