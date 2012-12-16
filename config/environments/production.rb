6920e5050aa3b8eb6fb4c8e48385d3fbe6e0bf9c
Matias Hernandez Arellano <matiasfh@gmail.com>
2012-12-16 00:49:25 -0300

    Remove Assets_sync

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,16 +43,16 @@ DandooDev::Application.configure do
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
-  config.cache_store = :dalli_store
+   config.cache_store = :dalli_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
+  #config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
 
   
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   #config.assets.precompile += %w(*.js,,active_admin.js,active_admin.css)
-  #config.assets.precompile += %w(templates/*,collections/*,models/*,views/*,*.js,images/*)
+  config.assets.precompile += %w(templates/*,collections/*,models/*,views/*,*.js,images/*)
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
